2a289d8d9ee5d34a50fb2305e67c4b0262d2f695
salvadorcernaneri <sal.cernaneri@mail.utoronto.ca>
2022-12-10 17:28:19 -0500

    created navbar component code from free template

--- PF/phase2/src/contextsSal/APIContext.js
+++ PF/phase2/src/contextsSal/APIContext.js
@@ -2,15 +2,19 @@ import {createContext, useState} from "react";
 
 export const useAPIContext = () => {
     const [attended, setAttended] = useState([]);
+    const [open, setOpen] = useState(true);
 
     return {
         attended,
-        setAttended
+        setAttended,
+        open,
+        setOpen
     }
 }
 
 const APIContext = createContext({
     attended: null, setAttended: () => {},
+    open: null, setOpen: () => {}
 })
 
 export default APIContext;
