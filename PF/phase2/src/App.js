8dab3ef5e4b77d6243cdcf1950afc1f2dc76599e
salvadorcernaneri <sal.cernaneri@mail.utoronto.ca>
2022-12-10 16:54:48 -0500

    edit

--- PF/phase2/src/App.js
+++ PF/phase2/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import SignUp from "./componentsSal/SignUp";
 import LogIn from "./componentsSal/Login";
+import Edit from "./componentsSal/Edit";
 import Dashboard from "./componentsSal/Classes";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import APIContext, {useAPIContext} from "./contextsSal/APIContext";
@@ -19,6 +20,7 @@ function App() {
             <Route index element={<SignUp />} />
               <Route path="login" element={<LogIn />} />
               <Route path="classes" element={classes} />
+              <Route path="edit" element={<Edit/>} />
           </Route>
         </Routes>
       </BrowserRouter>
